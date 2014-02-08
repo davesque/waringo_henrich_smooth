df536d6fc1f36a6ab131dbfa93a7dd5b7f9c803b
David Sanders <david@retul.com>
2014-02-07 19:29:09 -0700

    First commit

---
+++ smooth.py
@@ -0,0 +1,124 @@
+from __future__ import division
+
+from math import (
+    fabs,
+    pow,
+    sqrt,
+)
+import operator
+
+
+def point_to_point_distance(p1, p2):
+    """
+    Returns the distance between a point `p1` and a point `p2`.
+    """
+    if p1.x == p2.x and p1.y == p2.y:
+        # Special case for p1 == p2
+        return 0.0
+
+    if p1.x == p2.x:
+        # Special case for slope infinity
+        d = p2.y - p1.y
+    elif p1.y == p2.y:
+        # Special case for slope 0
+        d = p2.x - p1.x
+    else:
+        # Normal case
+        d = sqrt(pow(p2.y - p1.y, 2) + pow(p2.x - p1.x, 2))
+
+    return fabs(d)
+
+
+def point_to_line_distance(p1, p2, p3):
+    """
+    Returns the distance between a point `p2` and the line formed by two points
+    `p1` and `p3`.
+    """
+    if (
+        p1.x == p2.x and
+        p1.y == p2.y and
+        p2.x == p3.x and
+        p2.y == p3.y
+    ):
+        # Special case for p1 == p2 == p3
+        return 0
+
+    if p1.x == p3.x and p1.y == p3.y:
+        # Special case for p1 == p3
+        return point_to_point_distance(p1, p2)
+
+    if p1.x == p3.x:
+        # Special case for slope infinity
+        d = p2.x - p1.x
+    elif p1.y == p3.y:
+        # Special case for slope 0
+        d = p2.y - p1.y
+    else:
+        # Normal case
+        m = (p3.y - p1.y) / (p3.x - p1.x)
+        b = p1.y - m * p1.x
+        d = (p2.y - m * p2.x - b) / sqrt(m * m + 1)
+
+    return fabs(d)
+
+
+"""
+/**
+ * Finds the indexes of neighboring, non-removed points for the point `p` in
+ * the point list `points`.
+ */
+function findNeighborhood(points, p) {
+  var removed, left, right, i, j, k;
+
+  if ( _.isEmpty(points) ) return [null, null];
+
+  j = p.i;
+  removed = _.pluck(points, "r");
+
+  left = _.first(removed, j);
+  right = _.rest(removed, j + 1);
+
+  i = _.lastIndexOf(left, false);
+  k = _.indexOf(right, false);
+
+  if ( k !== -1 ) k += j + 1;
+
+  return [i === -1 ? null : points[i], k === -1 ? null : points[k]];
+}
+"""
+
+
+def find_neighborhood(points, p):
+    """
+    Finds neighboring, non-removed points for the point `p` in the point list
+    `points`.
+    """
+    if not points:
+        return (None, None)
+
+    points_len = len(points)
+    left_point = right_point = None
+
+    # Find closest neighbor to the left
+    i = p.i - 1
+    while True:
+        if i < 0:
+            break
+        point = points[i]
+        if point.r is False:
+            left_point = point
+            break
+        i -= 1
+
+    # Find closest neighbor to the right
+    i = p.i + 1
+    while True:
+        if i >= points_len:
+            break
+        point = points[i]
+        if point.r is False:
+            right_point = point
+            break
+        i += 1
+
+    return (left_point, right_point)
---
+++ smooth_tests.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+
+import unittest
+import collections
+import math
+
+from smooth import (
+    point_to_point_distance, point_to_line_distance, find_neighborhood,
+)
+
+
+Point = collections.namedtuple('Point', ['x', 'y'])
+AnnotatedPoint = collections.namedtuple('Point', ['x', 'y', 'i', 'r'])
+
+
+class PointToPointDistanceTest(unittest.TestCase):
+    def test_it_should_find_the_correct_distance_when_p1_equals_p2(self):
+        self.assertEqual(point_to_point_distance(
+            Point(0, 0),
+            Point(0, 0),
+        ), 0)
+        self.assertEqual(point_to_point_distance(
+            Point(1, 1),
+            Point(1, 1),
+        ), 0)
+
+    def test_it_should_find_the_correct_distance_when_the_line_slope_is_infinity(self):
+        self.assertEqual(point_to_point_distance(
+            Point(0, 0),
+            Point(0, 10),
+        ), 10)
+        self.assertEqual(point_to_point_distance(
+            Point(123, 0),
+            Point(123, -10),
+        ), 10)
+
+    def test_it_should_find_the_correct_distance_when_the_line_slope_is_0(self):
+        self.assertEqual(point_to_point_distance(
+            Point(0, 0),
+            Point(10, 0),
+        ), 10)
+        self.assertEqual(point_to_point_distance(
+            Point(0, -10),
+            Point(1000, -10),
+        ), 1000)
+
+    def test_it_should_find_the_correct_distance_in_a_normal_case(self):
+        self.assertAlmostEqual(point_to_point_distance(
+            Point(0, 0),
+            Point(10, 10),
+        ), 14.1421, 4)
+        self.assertAlmostEqual(point_to_point_distance(
+            Point(10, -5),
+            Point(-10, 5),
+        ), 22.3607, 4)
+
+
+class PointToLineDistanceTest(unittest.TestCase):
+    def test_it_should_find_the_correct_distance_when_the_line_slope_is_infinity(self):
+        self.assertEqual(point_to_line_distance(
+            Point(0, 0),
+            Point(5, -100),
+            Point(0, 10),
+        ), 5)
+        self.assertEqual(point_to_line_distance(
+            Point(-20, -2000),
+            Point(5, 0),
+            Point(-20, 10),
+        ), 25)
+
+    def test_it_should_find_the_correct_distance_when_the_line_slope_is_0(self):
+        self.assertEqual(point_to_line_distance(
+            Point(0, 0),
+            Point(5, 1000),
+            Point(10, 0),
+        ), 1000)
+        self.assertEqual(point_to_line_distance(
+            Point(0, -20),
+            Point(5, 1000),
+            Point(-20, -20),
+        ), 1020)
+
+    def test_it_should_find_the_correct_distance_when_the_line_has_length_0(self):
+        self.assertEqual(point_to_line_distance(
+            Point(0, 0),
+            Point(5, 0),
+            Point(0, 0),
+        ), 5)
+        self.assertEqual(point_to_line_distance(
+            Point(-10, -10),
+            Point(10, 10),
+            Point(-10, -10),
+        ), math.sqrt(20.0 * 20.0 + 20.0 * 20.0))
+
+    def test_it_should_find_the_correct_distance_when_p1_equals_p2_equals_p3(self):
+        self.assertEqual(point_to_line_distance(
+            Point(0, 0),
+            Point(0, 0),
+            Point(0, 0),
+        ), 0)
+        self.assertEqual(point_to_line_distance(
+            Point(1, 1),
+            Point(1, 1),
+            Point(1, 1),
+        ), 0)
+        self.assertEqual(point_to_line_distance(
+            Point(-1, -1),
+            Point(-1, -1),
+            Point(-1, -1),
+        ), 0)
+
+    def test_it_should_find_the_correct_distance_in_a_normal_case(self):
+        self.assertAlmostEqual(point_to_line_distance(
+            Point(0, 0),
+            Point(0, 1),
+            Point(1, 1),
+        ), 0.7071, 4)
+        self.assertAlmostEqual(point_to_line_distance(
+            Point(0, 0),
+            Point(5, 1),
+            Point(10, 1),
+        ), 0.4975, 4)
+        self.assertAlmostEqual(point_to_line_distance(
+            Point(0, 0),
+            Point(6, 1.1),
+            Point(10, 1),
+        ), 0.4975, 4)
+        self.assertAlmostEqual(point_to_line_distance(
+            Point(-10, 10),
+            Point(-4, 8),
+            Point(-3, 5),
+        ), 1.8600, 4)
+
+
+class FindNeighborhoodTestCase(unittest.TestCase):
+    def setUp(self):
+        self.coords1 = [
+            AnnotatedPoint(0, 0, 0, True),
+            AnnotatedPoint(0, 0, 1, True),
+            AnnotatedPoint(0, 0, 2, False),
+            AnnotatedPoint(0, 0, 3, True),
+            AnnotatedPoint(0, 0, 4, False),
+            AnnotatedPoint(0, 0, 5, False),
+            AnnotatedPoint(0, 0, 6, True),
+        ]
+        self.coords2 = [
+            AnnotatedPoint(0, 0, 0, True),
+            AnnotatedPoint(0, 0, 1, True),
+            AnnotatedPoint(0, 0, 2, False),
+            AnnotatedPoint(0, 0, 3, True),
+            AnnotatedPoint(0, 0, 4, True),
+            AnnotatedPoint(0, 0, 5, False),
+            AnnotatedPoint(0, 0, 6, True),
+            AnnotatedPoint(0, 0, 7, False),
+            AnnotatedPoint(0, 0, 8, False),
+            AnnotatedPoint(0, 0, 9, True),
+            AnnotatedPoint(0, 0, 10, False),
+            AnnotatedPoint(0, 0, 11, True),
+            AnnotatedPoint(0, 0, 12, True)
+        ]
+
+    def test_it_should_find_the_closest_points_on_the_left_and_right_which_will_not_be_removed(self):
+        self.assertEqual(find_neighborhood(self.coords1, self.coords1[3]), (self.coords1[2], self.coords1[4]))
+        self.assertEqual(find_neighborhood(self.coords1, self.coords1[4]), (self.coords1[2], self.coords1[5]))
+        self.assertEqual(find_neighborhood(self.coords2, self.coords2[5]), (self.coords2[2], self.coords2[7]))
+        self.assertEqual(find_neighborhood(self.coords2, self.coords2[8]), (self.coords2[7], self.coords2[10]))
+
+    def test_it_should_return_none_as_the_right_point_if_no_valid_point_on_the_right_is_found(self):
+        self.assertEqual(find_neighborhood(self.coords1, self.coords1[5]), (self.coords1[4], None))
+        self.assertEqual(find_neighborhood(self.coords2, self.coords2[10]), (self.coords2[8], None))
+
+    def test_it_should_return_none_as_the_left_point_if_no_valid_point_on_the_left_is_found(self):
+        self.assertEqual(find_neighborhood(self.coords1, self.coords1[1]), (None, self.coords1[2]))
+        self.assertEqual(find_neighborhood(self.coords2, self.coords2[2]), (None, self.coords2[5]))
+
+    def test_it_should_return_none_as_the_right_point_if_the_given_point_is_the_last_point(self):
+        self.assertEqual(find_neighborhood(self.coords1, self.coords1[6]), (self.coords1[5], None))
+        self.assertEqual(find_neighborhood(self.coords2, self.coords2[12]), (self.coords2[10], None))
+
+    def test_it_should_return_none_as_the_left_point_if_the_given_point_is_the_first_point(self):
+        self.assertEqual(find_neighborhood(self.coords1, self.coords1[0]), (None, self.coords1[2]))
+        self.assertEqual(find_neighborhood(self.coords2, self.coords2[0]), (None, self.coords2[2]))
+
+    def test_it_should_return_none_for_both_the_left_and_right_points_if_the_given_list_is_empty(self):
+        self.assertEqual(find_neighborhood([], None), (None, None))
+        self.assertEqual(find_neighborhood([], None), (None, None))
+
+    def test_it_should_return_none_for_both_the_left_and_right_points_if_the_given_has_a_length_of_1(self):
+        coords = [AnnotatedPoint(0, 0, 0, False)]
+        self.assertEqual(find_neighborhood(coords, coords[0]), (None, None))
+
+
+if __name__ == '__main__':
+    unittest.main()
